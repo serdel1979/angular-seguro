553b39087ee09679593e0fac545b4912f8a04823
unknown <sdlbsso@gmail.com>
2022-09-28 11:31:13 -0300

    Seguridad en angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Security</h1>
+  <app-inner-html-binding></app-inner-html-binding>
+  <app-bypass-security></app-bypass-security>
+  `
+})
+export class AppComponent {
+}
